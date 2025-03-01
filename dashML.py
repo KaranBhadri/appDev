44b1346aa94036684e182a526f704528ea3d9d77
Karan Bhadri <58948705+KaranBhadri@users.noreply.github.com>
2025-03-01 15:39:35 -0800

    Update dashML.py

--- dashML.py
+++ dashML.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🏆 XPLAINABLE MODEL')
 st.write("Hello World")
+
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+df
+
