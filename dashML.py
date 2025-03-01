5a219c8a099a04330ce090bec1fba0bb8d79bfae
Karan Bhadri <58948705+KaranBhadri@users.noreply.github.com>
2025-03-01 15:40:41 -0800

    Update dashML.py

--- dashML.py
+++ dashML.py
@@ -4,7 +4,8 @@ import pandas as pd
 st.title('🏆 XPLAINABLE MODEL')
 st.write("Hello World")
 
-
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
-df
+with st.expander('**DATA**'):
+  st.write('**input data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+  df
 
