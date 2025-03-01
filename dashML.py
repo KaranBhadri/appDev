6d48c6442b1f87d9e14a72b56bcbe4432cde301b
Karan Bhadri <58948705+KaranBhadri@users.noreply.github.com>
2025-03-01 15:47:01 -0800

    Update dashML.py

--- dashML.py
+++ dashML.py
@@ -18,5 +18,5 @@ with st.expander('**DATA**'):
   Y
 
 with st.expander('**VISUALIZATIONS**'):
-  st.scatter_plot(df, X=species, y=body_mass_g)
+  st.scatter_plot(df, X="species", y="body_mass_g")
 
